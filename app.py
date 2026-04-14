2adec715e9f7c5fef6d87042382103cf3404ecee
designprojectgroup100-ai <designprojectgroup100@gmail.com>
2026-04-15 03:59:45 +0530

    app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ from predictor import predict_price
 st.set_page_config(page_title="Real Estate Analyzer", layout="wide")
  
 st.title("🏡 Indian Real Estate Price Analyzer")
-st.subheader("📊 Model Performance")
-st.write(f"R² Score: {score}")
- 
  
 # Load & preprocess data
 df = load_raw_data()
@@ -21,6 +18,10 @@ location, bhk, sqft, budget = sidebar_controls(df)
  
 # Train model
 model, columns, score = train_model(df)
+
+#display score
+st.subheader("📊 Model Performance")
+st.write(f"R² Score: {score}")
  
 # Prediction
 price = predict_price(model, columns, location, bhk, sqft)
